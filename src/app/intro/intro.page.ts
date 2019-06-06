c8a992574d44e5a0197b1894e6bf3e8cbaedea56
RafaTakashi <usagitk@gmail.com>
2019-06-06 18:32:08 -0300

    first commit

---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
